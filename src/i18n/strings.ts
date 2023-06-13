892727e1d9d838eb28caab324c5a4e9f076f2447
cariac <iacopo.carlini@zucchetti.it>
2023-06-13 17:56:26 +0200

    Aggiunta mappa e review layout

---
+++ src/i18n/strings.ts
@@ -0,0 +1,18 @@
+export const languages = {
+    en: 'English',
+    it: 'Italian',
+  };
+  
+  export const defaultLang = 'it';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.about': 'About',
+      'nav.twitter': 'Twitter',
+    },
+    it: {
+      'nav.home': 'Acceuil',
+      'nav.about': 'À propos',
+    },
+  } as const;
