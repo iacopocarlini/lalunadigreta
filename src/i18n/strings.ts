46fed56b987114d408e2a59e89bea9562f9ae84c
cariac <iacopo.carlini@zucchetti.it>
2023-06-14 11:17:23 +0200

    Aggiunto meccanismo traduzioni

--- src/i18n/strings.ts
+++ src/i18n/strings.ts
@@ -1,18 +1,20 @@
 export const languages = {
-    en: 'English',
-    it: 'Italian',
-  };
+  it: 'ITA',  
+  en: 'ENG',
+};
   
   export const defaultLang = 'it';
   
   export const ui = {
-    en: {
-      'nav.home': 'Home',
-      'nav.about': 'About',
-      'nav.twitter': 'Twitter',
-    },
+
+    // ITALIAN
     it: {
-      'nav.home': 'Acceuil',
-      'nav.about': 'À propos',
+      'Landing.h1': 'Agriturismo a Bardine di San Terenzo (MS)',
     },
+
+    // ENGLISH
+    en: {
+      'Landing.h1': 'Agriturismo in Bardine, Tuscany',
+    },
+    
   } as const;
