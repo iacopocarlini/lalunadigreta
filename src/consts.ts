e15096cd472e113e54499d8795e01b413eeaf3e7
cariac <iacopo.carlini@zucchetti.it>
2023-06-15 15:10:11 +0200

    Carosello menu e testi

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,10 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// Global data here
 export const SITE_TITLE = 'LA LUNA DI GRETA';
 export const SITE_DESCRIPTION = 'Agriturismo a Bardine di San Terenzo';
-export const PHONE = '+393341871889';
+export const PHONE = '+393669778197';
 export const EMAIL = 'test@pippopaolo.it';
 export const AUTHOR = 'Iacopo Carlini';
 export const AUTHOR_LINK = 'https://iacopocarlini.github.io/';
 export const FBLINK = 'https://www.facebook.com/lunadigreta/';
+export const INSTAGRAM_LINK = 'https://www.instagram.com/lalunadigreta/';
 export const TRIPADVLINK = 'https://www.tripadvisor.it/Restaurant_Review-g1426369-d23606550-Reviews-Agriturismo_La_Luna_Di_Greta-Fivizzano_Province_of_Massa_Carrara_Tuscany.html';
