4632dc350bdb11d2ae3bd8dc842f0c266f6e2b8c
cariac <iacopo.carlini@zucchetti.it>
2023-06-19 11:26:29 +0200

    Aggiunto logo e restyling

--- src/i18n/strings.ts
+++ src/i18n/strings.ts
@@ -9,12 +9,66 @@ export const languages = {
 
     // ITALIAN
     it: {
-      'Landing.h1': 'Agriturismo a Bardine di San Terenzo (MS)',
+      'Landing.h1': 'Agriturismo a Bardine di San Terenzo, Toscana',
+      'Presentation.h1': 'CHI SIAMO',
+      
+      'Presentation.p': `Situato in posizione privilegiata nella campagna
+      dell'alta Lunigiana, immerso in un'oasi verde, a pochi passi
+      dai boschi di castagni e querce, nelle vicinanze di un torrente,
+      sorge il nostro Agriturismo frutto della passione di quattro 
+      generazioni per la campagna. La nostra struttura offre prodotti tipici,
+      ristorante con cibi fatti in casa con ingredienti frutto del nostro lavoro, 
+      e un parco per bambini.`,
+      
+      'Menu.h1': "LE NOSTRE SPECIALITA'",
+      'Menu.p1': `
+        L'azienda agricola produce olio, mele rotelle, ortaggi, frutta e verdura, 
+        carne di pollo, tacchino, coniglio e maiale, uova fresche. Alleviamo anche avicoli cunicoli ornamentali.
+        Da noi potete acquistare i prodotti freschi e vedere la nostra produzione`,
+      'Menu.p2': `Offriamo la possibilità di gustare piatti della terra di Lunigiana direttamente nei luoghi da cui provengono e preparati alla vecchia maniera, 
+      tutte le pietanze sono cucinate con prodotti propri o delle aziende lunigianesi.`,
+      'Menu.p3-title': 'Alcuni dei nostri primi:',
+      'Menu.p3': `ravioli di carne al ragù, ravioli alle erbette
+      e ricotta, lasagne bianche o verdi al forno, gnocchi e gnocchetti, tortelloni
+      di patate o zucca, tagliatelle, minestrone...`,
+      'Menu.p4-title': 'Alcuni dei nostri secondi:',
+      'Menu.p4':`pollo e tacchino al forno, cinta senese al forno,
+      arista con crema di verdure, involtini in agrodolce, coniglio fritto o alla
+      cacciatore, agnello al forno o fritto, cacciagione, maialino al forno...`, 
+
+      'Map.h1': 'DOVE SIAMO',
+
+      'Contacts.h1': 'CONTATTI',
+      'Contacts.phone': 'Tel:',
+      'Contacts.email': 'Email:',
     },
 
     // ENGLISH
     en: {
       'Landing.h1': 'Agriturismo in Bardine, Tuscany',
+
+      'Presentation.h1': 'ABOUT',
+
+      'Presentation.p': `Placed in a green oasis in the heart of Lunigiana, our Agriturismo is the result of a
+      4 generation passion for the countryside life. Our facility offers local products, home made cuisine restaurant,
+      a playground for kids and much more.`,
+
+      'Menu.h1': 'DELICATESSEN',
+      'Menu.p1': `Our farm produces oil, apples, groceries, meats and fresh eggs. While staying at the Agriturismo, 
+      you can buy our fresh products or watch the making process.`,
+      'Menu.p2': `Here you can taste the original old style Lunigiana cuisine. 
+      Every dish is made using fresh ingredients from our farm or other Lunigiana farms. `,
+      'Menu.p3-title': 'Some of our main courses:',
+      'Menu.p3': `Ravioli in ragù or herbs sauce, white or green lasagna, gnocchi and gnocchetti, tagliatelle, minestrone`,
+      'Menu.p4-title': 'Some of our second courses:',
+      'Menu.p4':`roasted chicken, turkey or piglet, roasted typical "cinta senese", fried rabbit or lamb`,       
+
+      'Map.h1': 'FIND US',
+
+      'Contacts.h1': 'CONTACT US',
+      'Contacts.phone': 'Phone:',
+      'Contacts.email': 'Email:',
+      
     },
     
   } as const;
