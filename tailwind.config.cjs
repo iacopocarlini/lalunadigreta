617b328775768a2b87ae81920e544e5ae62be866
cariac <iacopo.carlini@zucchetti.it>
2023-06-20 10:51:18 +0200

    Rimosso flowbite, aggiunto swiper e refactorign

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,10 @@
 module.exports = {
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js'
 	],
 	theme: {
 		extend: {},
 	},
 	plugins: [
-		require('flowbite/plugin')
 	],
 }
