8bc1f2763a8ee8e7dff73ba0246ad448dda96f2c
cariac <iacopo.carlini@zucchetti.it>
2023-06-29 23:53:08 +0200

    Deploy e dominio custom

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://lalunadigreta.it',
   integrations: [mdx(), sitemap(), tailwind()]
 });
